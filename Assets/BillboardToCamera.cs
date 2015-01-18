e954571fe1c19f4adfa0fe63efee80b6890baf25
Kevin Cox <kevin.cox@dena.com>
2015-01-18 03:03:14 -0800

    initial

---
+++ Assets/BillboardToCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Either drop this on a GameObject you want to billboard in the editor, or call
+// BillboardToCamera.Billboard(this.transform); from other MonoBehaviours
+public class BillboardToCamera : MonoBehaviour {
+
+	void Update() {
+		Billboard(transform);
+	}
+
+	public static void Billboard(Transform transform) {
+		Transform cameraTransform = Camera.main.transform;
+		transform.rotation = cameraTransform.rotation;
+	}
+}
---
+++ Assets/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Rotate : MonoBehaviour {
+
+	public float rate = 5f;
+	
+	void Update() {
+		this.transform.rotation = Quaternion.Euler(0, rate * Time.time, 0f);
+	}
+}
